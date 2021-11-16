b42fa305b820983570f479355ecd6e6b345ad5b8
jc <jc3158@georgetwon.edu>
2021-11-17 03:02:00 +0800

    sdd

---
+++ HW6.0/HW6.0A2.py
@@ -0,0 +1,162 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 17 01:09:04 2021
+
+@author: Lenovo
+"""
+
+
+## deep 
+
+
+#SOURCE: MODIFIED FROM https://blog.keras.io/building-autoencoders-in-keras.html
+
+import keras
+from keras import layers
+import matplotlib.pyplot as plt
+
+from keras.datasets import mnist,cifar10,fashion_mnist
+import numpy as np
+
+#USER PARAM
+INJECT_NOISE    =   False
+EPOCHS          =   35
+NKEEP           =   2500        #DOWNSIZE DATASET
+BATCH_SIZE      =   128
+DATA            =   "FASION_MNIST"
+
+
+#GET DATA
+if(DATA=="MNIST"):
+    (x_train, _), (x_test, _) = mnist.load_data()
+    N_channels=1; PIX=28
+
+if(DATA=="CIFAR"):
+    (x_train, _), (x_test, _) = cifar10.load_data()
+    N_channels=3; PIX=32
+    EPOCHS=100 #OVERWRITE
+if(DATA=="FASION_MNIST"):
+    (x_train, _), (x_test, _) = fashion_mnist.load_data()
+    N_channels=1; PIX=28
+#NORMALIZE AND RESHAPE
+x_train = x_train.astype('float32') / 255.
+x_test = x_test.astype('float32') / 255.
+
+x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
+x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
+
+
+
+#ADD NOISE IF DENOISING
+if(INJECT_NOISE):
+    EPOCHS=2*EPOCHS
+    #GENERATE NOISE
+    noise_factor = 0.5
+    noise= noise_factor * np.random.normal(loc=0.0, scale=1.0, size=x_train.shape) 
+    x_train=x_train+noise
+    noise= noise_factor * np.random.normal(loc=0.0, scale=1.0, size=x_test.shape) 
+    x_test=x_test+noise
+
+    #CLIP ANY PIXELS OUTSIDE 0-1 RANGE
+    x_train = np.clip(x_train, 0., 1.)
+    x_test = np.clip(x_test, 0., 1.)
+
+#BUILD CNN-AE MODEL
+
+
+if(DATA=="MNIST"):
+
+
+  
+
+
+    input_img = keras.Input(shape=(784,))
+    encoded = layers.Dense(128, activation='relu')(input_img)
+    encoded = layers.Dense(64, activation='relu')(encoded)
+    encoded = layers.Dense(32, activation='relu')(encoded)
+
+    decoded = layers.Dense(64, activation='relu')(encoded)
+    decoded = layers.Dense(128, activation='relu')(decoded)
+    decoded = layers.Dense(784, activation='sigmoid')(decoded)
+
+
+
+
+if(DATA=="CIFAR"):
+    input_img = keras.Input(shape=(PIX, PIX, N_channels))
+
+    #ENCODER
+    x = layers.Conv2D(32, (3, 3), activation='relu', padding='same')(input_img)
+    x = layers.MaxPooling2D((2, 2), padding='same')(x)
+    x = layers.Conv2D(16, (3, 3), activation='relu', padding='same')(x)
+    encoded = layers.MaxPooling2D((2, 2), padding='same')(x)
+
+    #DECODER
+    x = layers.Conv2D(16, (3, 3), activation='relu', padding='same')(encoded)
+    x = layers.UpSampling2D((2, 2))(x)
+    x = layers.Conv2D(32, (3, 3), activation='relu', padding='same')(x)
+    x = layers.UpSampling2D((2, 2))(x)
+    decoded = layers.Conv2D(N_channels, (3, 3), activation='sigmoid', padding='same')(x)
+
+
+if(DATA=="FASION_MNIST"):
+
+
+  
+
+
+    input_img = keras.Input(shape=(784,))
+    encoded = layers.Dense(128, activation='relu')(input_img)
+    encoded = layers.Dense(64, activation='relu')(encoded)
+    encoded = layers.Dense(32, activation='relu')(encoded)
+
+    decoded = layers.Dense(64, activation='relu')(encoded)
+    decoded = layers.Dense(128, activation='relu')(decoded)
+    decoded = layers.Dense(784, activation='sigmoid')(decoded)
+
+
+
+#COMPILE
+autoencoder = keras.Model(input_img, decoded)
+autoencoder.compile(optimizer='adam', loss='binary_crossentropy');
+autoencoder.summary()
+
+#TRAIN
+history = autoencoder.fit(x_train, x_train,
+                epochs=EPOCHS,
+                batch_size=BATCH_SIZE,
+                shuffle=True,
+                validation_data=(x_test, x_test),
+                )
+
+#HISTORY PLOT
+epochs = range(1, len(history.history['loss']) + 1)
+plt.figure()
+plt.plot(epochs, history.history['loss'], 'bo', label='Training loss')
+plt.plot(epochs, history.history['val_loss'], 'b', label='Validation loss')
+plt.legend()
+
+autoencoder.save("my_model2.h5")
+
+#MAKE PREDICTIONS FOR TEST DATA
+decoded_imgs = autoencoder.predict(x_test)
+
+#VISUALIZE THE RESULTS
+n = 10
+plt.figure(figsize=(20, 4))
+for i in range(1, n + 1):
+    # Display original
+    ax = plt.subplot(2, n, i)
+    plt.imshow(x_test[i].reshape(28, 28))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+    # Display reconstruction
+    ax = plt.subplot(2, n, i + n)
+    plt.imshow(decoded_imgs[i].reshape(28, 28))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+plt.show()
+
